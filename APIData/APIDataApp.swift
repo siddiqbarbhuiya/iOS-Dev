c3b0e018a2e3ba21dabfeb0873515c28184aa0f6
siddiqbarbhuiya <siddiqbarbhuiya@outlook.com>
2022-08-09 14:23:23 +0530

    Initial Commit

---
+++ APIData/APIDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIDataApp.swift
+//  APIData
+//
+//  Created by Siddiq Barbhuiya on 09/08/22.
+//
+
+import SwiftUI
+
+@main
+struct APIDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
