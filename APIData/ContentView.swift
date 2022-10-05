cd5f6edfa1ff6534b62f0cc7d35bd286e3eb5646
siddiqbarbhuiya <siddiqbarbhuiya@outlook.com>
2022-10-05 23:40:06 +0530

    first commit

--- APIData/ContentView.swift
+++ APIData/ContentView.swift
@@ -7,10 +7,54 @@
 
 import SwiftUI
 
+
+struct Quote: Codable {
+    var quote_id: Int
+    var quote: String
+    var author: String
+    var series: String
+}
+
 struct ContentView: View {
+    @State private var quotes = [Quote]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(quotes, id: \.quote_id) { quote in
+                VStack(alignment: .leading) {
+                    Text(quote.author)
+                        .font(.headline)
+                        .foregroundColor(Color("skyBlue"))
+                    
+                    Text(quote.quote)
+                        .font(.body)
+                        .foregroundColor(.secondary)
+                }
+            }
+            .navigationTitle("Quotes")
+            .task {
+                await fetchData()
+            }
+        }
+    }
+    func fetchData() async {
+        //create url
+        guard let url = URL(string: "https://www.breakingbadapi.com/api/quotes") else {
+            print("No Data Found")
+            return
+        }
+        
+        //fetch data
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            //decode that data
+            if let decodedResponse = try? JSONDecoder().decode([Quote].self, from: data) {
+                quotes = decodedResponse
+            }
+        } catch {
+            print("Error found")
+        }
     }
 }
 
