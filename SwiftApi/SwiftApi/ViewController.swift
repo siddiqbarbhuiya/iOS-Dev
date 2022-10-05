161268f1bc625dee64122db6b681d18ef577db42
siddiqbarbhuiya <siddiqbarbhuiya@outlook.com>
2022-10-05 23:51:51 +0530

    first commit

---
+++ SwiftApi/SwiftApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftApi
+//
+//  Created by Siddiq Barbhuiya on 03/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
